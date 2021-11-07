6cce31e158b512c83bae1d0418502822f947254b
KhaledAlabssi <khaled.absi5@gmail.com>
2021-11-07 10:30:12 +0100

    implementing React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Container, Navbar, Nav} from "react-bootstrap";
+
+import { Container, Navbar, Nav } from "react-bootstrap";
+// import { LinkContainer } from "react-router-bootstrap";
 
 export default function Header() {
   return (
     <div>
       <header>
-        <Navbar bg="primary" variant='light' expand="lg" collapseOnSelect>
+        <Navbar bg="primary" variant="light" expand="lg" collapseOnSelect>
           <Container>
-            <Navbar.Brand href="/">K-A E-commerce</Navbar.Brand>
+            {/* <LinkContainer exact to="/"> */}
+              <Navbar.Brand>K-A E-commerce</Navbar.Brand>
+            {/* </LinkContainer> */}
+
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
               <Nav
@@ -17,18 +22,17 @@ export default function Header() {
               >
                 {/* <Nav.Link href="/cart">Cart</Nav.Link>
                 <Nav.Link href="/login">Sign In</Nav.Link> */}
-                
-                
               </Nav>
-              <Nav
-              >
-                <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-                <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
-                
-                
+              <Nav>
+                {/* <Link to='/cart'><i className='fas fa-shopping-cart'></i> Cart</Link> */}
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+
+                <Nav.Link href="/login">
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
               </Nav>
-              
-              
             </Navbar.Collapse>
           </Container>
         </Navbar>
