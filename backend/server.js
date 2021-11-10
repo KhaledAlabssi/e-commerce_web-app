36265d45744c0dad360ecf04dbb8a5b578a8288a
KhaledAlabssi <khaled.absi5@gmail.com>
2021-11-10 17:38:03 +0100

    create products routes and fetch data from mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js';
-import products from './data/products.js'
+
+import productRoutes from './routes/ProductRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -12,14 +13,9 @@ app.get('/', (req, res) => {
     res.send('Backend is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
