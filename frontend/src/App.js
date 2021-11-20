ee4726757293e9f52e80a94c2ac844e95e7fe9aa
KhaledAlabssi <khaled.absi5@gmail.com>
2021-11-20 13:29:28 +0100

    add product and qty to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -13,8 +14,15 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
-          <Route path='/' element={<HomeScreen />} exact />
-          <Route path='/product/:id' element={<ProductScreen />} exact />
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} exact />
+            <Route path='cart'>
+              <Route path=':id' element={<CartScreen />} />
+
+              <Route path='' element={<CartScreen />} />
+
+            </Route>
+          
           </Routes>
 
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { listProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader'
@@ -9,16 +9,24 @@ import Message from '../components/Message'
 
 
 export default function ProductScreen() {
+    const [qty, setQty] = useState(0)
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
     const { loading, error, product} = productDetails
+    const navigate = useNavigate()
     
     let params = useParams();
     useEffect(() => {
         dispatch(listProductDetails(params.id))
         
         
-      }, [dispatch, params])
+    }, [dispatch, params])
+
+    function addToCartHandler() {
+        navigate(`/cart/${params.id}?qty=${qty}`)
+
+    }
+
 
       
     
@@ -51,6 +59,7 @@ export default function ProductScreen() {
 
                 </Col>
                 <Col md={3}>
+                    <Card>
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
                             <Row>
@@ -70,12 +79,30 @@ export default function ProductScreen() {
                             </Row>
 
                         </ListGroup.Item>
+                        {product.countInStock > 0 && (
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Qty</Col>
+                                    <Col>
+                                    <Form.Control as='select' value={qty} onChange={(e) => setQty(e.target.value)}>
+                                        {[...Array(product.countInStock).keys()].map(x => (
+                                            <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                        ))}
+
+                                    </Form.Control>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        )}
                         <ListGroup.Item>
-                            <Button className='btn-primary' type='button' disabled={product.countInStock <= 0}>
+                            <Button 
+                                onClick={addToCartHandler}
+                                className='btn-block' type='button' disabled={product.countInStock <= 0}>
                                 Add To Cart
                             </Button>
                         </ListGroup.Item>
                     </ListGroup>
+                    </Card>
 
                 </Col>
 
